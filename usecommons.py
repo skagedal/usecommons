1edd03e074dc4fca3ab0b13c044d6ebe705d84ea
Simon Kågedal Reimer <skagedal@gmail.com>
2012-03-08 15:00:51 +0100

    Add MIT/X11 license.

--- usecommons.py
+++ usecommons.py
@@ -1,3 +1,33 @@
+# Copyright (c) 2010-2011  Magnus Manske
+#           (c) 2011-2012  Krinkle
+#           (c) 2012-      Simon Kågedal Reimer
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#    This code is based on the MediaWiki "StockPhoto" gadget, written
+#    by Magnus Manske and Krinkle, converted to Python by Simon
+#    Kågedal Reimer.  Magnus and Krinkle agreed to license their code
+#    under the above MIT/X11 license: http://j.mp/stockphoto-license  
+#    License text from: http://copyfree.org/licenses/mit/license.txt
+
 # Standard Python libraries
 import sys, urllib, urllib2, os.path, json, re
 from distutils.dir_util import mkpath
